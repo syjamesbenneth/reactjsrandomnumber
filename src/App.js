dbd0d9a789d87b0cc2d95b9e2bea8c57f04ba95d
unknown <colonelspacepenguin@gmail.com>
2018-12-12 22:41:22 +0800

    react exercise

--- src/App.js
+++ src/App.js
@@ -8,9 +8,8 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <h2>Components Life Cycle</h2>
+          <Body />
           <a
             className="App-link"
             href="https://reactjs.org"
@@ -20,6 +19,74 @@ class App extends Component {
             Learn React
           </a>
         </header>
+      </div>
+    );
+  }
+}
+
+class Body extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      r:0
+    };
+    this.getRandomNumber = this.getRandomNumber.bind(this);
+  }
+
+  getRandomNumber(){
+    // console.log("random number called");
+
+    this.setState({r: Math.floor(Math.random()*10)});
+  }
+
+  render(){
+    return(
+      <div>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <button onClick={this.getRandomNumber}>Random Number</button>
+        <Numbers myNumber={this.state.r}/>
+      </div>
+    );
+  }
+}
+
+class Numbers extends Component {
+  componentWillMount(){
+    console.log("ComponentWillMount will be called here.");
+  }
+
+  componentDidMount(){
+    console.log("componentDidMount will be called here.");
+  }
+
+  componentWillReceiveProps(newProps){
+    console.log("ComponentWillReceiveProps will be called here.");
+  }
+
+  shouldComponentUpdate(newProps, nextState){
+    console.log("shouldComponentUpdate will be called here.");
+  }
+
+  componentWillUpdate(newProps, nextState){
+    console.log("componentWillUpdate will be called here.");
+  }
+
+  componentDidUpdate(newProps, nextState){
+    console.log("componentDidUpdate will be called here.");
+  }
+
+  componentWillUnmount(){
+    console.log("componentWillUnmount will be called here.");
+  }
+
+  render(){
+    return(
+      <div>
+        <br />
+        {this.props.myNumber}
       </div>
     );
   }
